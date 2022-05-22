70ec070d83a5b9b156b17bf595afba39e9e6eb60
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-05-21 20:43:44 -0500

    Global vars added.

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,14 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// variable letters, numbers, & special characters
+var chars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
